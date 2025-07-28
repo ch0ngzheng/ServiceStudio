75c6034d1561be2d846cb21f2e8731a3e76b4b7f
Jatlys <jatlyson@gmail.com>
2025-07-29 03:08:12 +0800

    Removed homepage Test file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import HomePage1 from './pages/HomePage1';
 import TransactionPage from './pages/transactionPage';
 import Login from './pages/Login';
 import './App.css'; 
@@ -15,7 +14,6 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/transactionPage" element={<TransactionPage />} />
         <Route path="/homePage/:userId" element={<HomePage />} />
-        <Route path="/homePage1/:userId" element={<HomePage1 />} />
         <Route path="/pay/:userId" element={<PayBannerPage />} />
         <Route path="/savings/:userId" element={<SavingsBannerPage />} />
         <Route path="/bto/:userId" element={<BTOBannerPage />} />
