1a60c1fa0d30c2201f4e4426e00a458d710cc774
Raihanah <151816916+pewds20@users.noreply.github.com>
2025-07-29 03:47:54 +0800

    fixed routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate, useParams } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import HomePage1 from './pages/HomePage1';
 import TransactionPage from './pages/transactionPage';
 import Login from './pages/Login';
 import './App.css';
@@ -8,7 +7,20 @@ import './App.css';
 import PayBannerPage from './pages/PayBannerPage.jsx';
 import SavingsBannerPage from './pages/SavingsBannerPage.jsx';
 import BTOBannerPage from './pages/BTOBannerPage.jsx';
+import SmartAdjustments from './pages/smartadjustments.jsx';
 import InsightsPage from './pages/InsightsPage';
+
+// Wrapper component for SmartAdjustments with navigation
+const SmartAdjustmentsWrapper = () => {
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  const handleNavigateToInsights = () => {
+    navigate(`/insights/${userId}`);
+  };
+
+  return <SmartAdjustments onNavigateToFriend={handleNavigateToInsights} />;
+};
 
 function App() {
   return (
@@ -17,12 +29,10 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/transactionPage" element={<TransactionPage />} />
         <Route path="/homePage/:userId" element={<HomePage />} />
-        <Route path="/homePage1/:userId" element={<HomePage1 />} />
         <Route path="/pay/:userId" element={<PayBannerPage />} />
         <Route path="/savings/:userId" element={<SavingsBannerPage />} />
         <Route path="/bto/:userId" element={<BTOBannerPage />} />
-        <Route path="/smartadjustments/:userId" element={<SmartAdjustments />} />  {/* Add this route */}
-        <Route path="/transactionPage" element={<TransactionPage />} />
+        <Route path="/smartadjustments/:userId" element={<SmartAdjustmentsWrapper />} />
         <Route path="/insights/:userId" element={<InsightsPage />} />
       </Routes>
     </Router>
