69f0ae73ab32de5e26e4ae6ce754118eaaed80fb
ch0ngzheng <chongzheng9@gmail.com>
2025-08-04 15:03:09 +0800

    fixed a bug of 2 card reco in insights screen

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -113,7 +113,7 @@ const HomePage = () => {
       {/* Header + Banner Section - Layered */}
       <div className="relative w-full">
         {/* Banner takes up the full space */}
-                <BannerComponent userId={userId} transportSpending={transportSpending} />
+                <BannerComponent userId={userId} />
         {/* Header is overlaid on top */}
         <div className="absolute inset-0 flex flex-col z-10">
           <DBSHeader 
